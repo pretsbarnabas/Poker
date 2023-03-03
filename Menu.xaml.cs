6953fc6b6558d99023611cf9289629ecf2a4634d
Tiszolczi Jácint <tiszolczi.jacint@students.jedlik.eu>
2023-03-03 12:26:46 +0100

    settings

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -20,8 +20,12 @@ namespace Poker
     /// </summary>
     public partial class Menu : Page
     {
+            public Dictionary<string, int> settings = new Dictionary<string, int>();
         public Menu()
         {
+            settings.Add("Zsetonok", 2000);     //default settings go here
+
+
             InitializeComponent();
         }
 
@@ -33,6 +37,43 @@ namespace Poker
         private void Close(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+         public void Settings(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Children.Clear();
+            
+            Label Zsetonok = new Label();
+            Zsetonok.Content = "Zsetonok";
+            Zsetonok.HorizontalAlignment = HorizontalAlignment.Left;
+            SettingsPanel.Children.Add(Zsetonok);
+            TextBox ZsetonSzam = new TextBox();
+            ZsetonSzam.HorizontalAlignment= HorizontalAlignment.Right;
+            ZsetonSzam.Text = settings["Zsetonok"].ToString();
+            int ZsetonDB = int.Parse(ZsetonSzam.Text);
+            ZsetonSzam.TextChanged += (sender, e) => ZsetonChanged(sender, e, ZsetonDB);
+            SettingsPanel.Children.Add(ZsetonSzam);
+
+            Button CloseButton = new Button();
+            CloseButton.Content = "Close";
+            CloseButton.Width = 80;
+            CloseButton.Height = 20;
+            CloseButton.VerticalAlignment = VerticalAlignment.Bottom;
+            CloseButton.HorizontalAlignment = HorizontalAlignment.Left;
+            CloseButton.AddHandler(Button.ClickEvent, new RoutedEventHandler(CloseButtonClick)); 
+            SettingsPanel.Children.Add(CloseButton);
+
+            SettingsPanel.Visibility = Visibility.Visible;
+        }
+
+        private void ZsetonChanged(object sender, RoutedEventArgs e, int zsetonDB)
+        {
+            settings["Zsetonok"] = zsetonDB;
+        }
+
+        void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility= Visibility.Collapsed;
         }
     }
 }
