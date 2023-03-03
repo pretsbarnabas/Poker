69a500b3c6e58b64e0e67ca64f905dd5433d52e9
Tiszolczi Jácint <tiszolczi.jacint@students.jedlik.eu>
2023-03-03 10:24:57 +0100

    Kezdokepernyo

---
+++ Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Poker
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Play(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Game.xaml", UriKind.Relative));
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
